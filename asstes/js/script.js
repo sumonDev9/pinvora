2fb74f68550211b5146436e72527169bc6d88db5
sucharita4867 <sucharitasardar7@gmail.com>
2026-09-16 18:10:33 +0530

    footer complete

---
+++ asstes/js/script.js
@@ -0,0 +1,8 @@
+// navbar toggole
+
+const menuBtn = document.getElementById("mobile-menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
